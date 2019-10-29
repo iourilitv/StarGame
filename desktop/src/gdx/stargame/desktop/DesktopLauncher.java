64211c00f0421ea648ea58b17cb431581073a0bd
iourilitv <iourilitv1965@gmail.com>
2019-10-29 14:38:39 +0300

    Initial commit

---
+++ desktop/src/gdx/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gdx.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gdx.stargame.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
