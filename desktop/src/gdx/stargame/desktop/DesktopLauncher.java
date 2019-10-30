96227390f6673b39e6164b3eab6384ff3dab94b6
iourilitv <iourilitv1965@gmail.com>
2019-10-30 14:20:38 +0300

    Completed the hw task of lesson 1.

--- desktop/src/gdx/stargame/desktop/DesktopLauncher.java
+++ desktop/src/gdx/stargame/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import gdx.stargame.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//устанавливаем размеры окна приложения
+		config.width = 512;
+		config.height = 1024;
+		//запрещаем изменение размеров окна приложения
+		config.resizable = false;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
