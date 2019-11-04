1794e3bbfa4bb10c1424c90413e5613adf699d45
iourilitv <iourilitv1965@gmail.com>
2019-11-04 10:49:22 +0300

    Made correction in the task2 and basically completed task3 of lesson 2 to hw.

--- core/src/gdx/stargame/lessons/lesson2/hw/screen/MenuScreen.java
+++ core/src/gdx/stargame/lessons/lesson2/hw/screen/MenuScreen.java
@@ -24,8 +24,10 @@ public class MenuScreen extends BaseScreen {
     private Vector2 v;
     //объявляем переменную вектора ускорения(acceleration)
     private final float SPEED_FACTOR = 2f;
-
-    //private Direction forward = Direction.FORWARD;
+    //объявляем переменную вида управления движением клавишами
+    private boolean keyControl;
+    //объявляем константу точности позиционирования
+    private final float PRECISION = 0.5f;
 
     @Override
     public void show() {
@@ -43,13 +45,13 @@ public class MenuScreen extends BaseScreen {
         posDest = new Vector2();
         v = new Vector2();
 
-        System.out.println("Direction.FORWARD.keyCode()=" + Direction.FORWARD.keyCode());
-        System.out.println("Direction.BACK.keyCode()=" + Direction.BACK.keyCode());
-        System.out.println("Direction.LEFT.keyCode()=" + Direction.LEFT.keyCode());
-        System.out.println("Direction.RIGHT.keyCode()=" + Direction.RIGHT.keyCode());
-
-        Direction.RIGHT.setKeyCode(99);
-        System.out.println("Direction.RIGHT.keyCode()=" + Direction.RIGHT.keyCode());
+//        System.out.println("Direction.FORWARD.keyCode()=" + Direction.FORWARD.keyCode());
+//        System.out.println("Direction.BACK.keyCode()=" + Direction.BACK.keyCode());
+//        System.out.println("Direction.LEFT.keyCode()=" + Direction.LEFT.keyCode());
+//        System.out.println("Direction.RIGHT.keyCode()=" + Direction.RIGHT.keyCode());
+//
+//        Direction.RIGHT.setKeyCode(99);
+//        System.out.println("Direction.RIGHT.keyCode()=" + Direction.RIGHT.keyCode());
 
     }
 
@@ -73,11 +75,39 @@ public class MenuScreen extends BaseScreen {
             pos.add(v);
         }*/
         //TODO L2hw.Added
-        //ограничиваем движение точкой назначения
-        if (Math.abs((int)(posDest.x - pos.x)) > 0 && Math.abs((int)(posDest.y - pos.y)) > 0) {
-            //пересчитываем позицию объекта для следующей итерации
-            // к вектору позиции прибавляем вектор скорости при каждом обновлении экрана
-            pos.add(v);
+        //если управление осуществляется мышью или тачэкраном
+        if(!keyControl){
+//            if (Math.abs((int)(posDest.x - pos.x)) > 0 && Math.abs((int)(posDest.y - pos.y)) > 0) {
+//                //пересчитываем позицию объекта для следующей итерации
+//                // к вектору позиции прибавляем вектор скорости при каждом обновлении экрана
+//                pos.add(v);
+//            }
+//            if (Math.abs(Math.round((posDest.x - pos.x) * PRECISION)) > PRECISION &&
+//                    Math.abs(Math.round((posDest.y - pos.y) * PRECISION)) > PRECISION) {
+//                //пересчитываем позицию объекта для следующей итерации
+//                // к вектору позиции прибавляем вектор скорости при каждом обновлении экрана
+//                pos.add(v);
+//            }
+
+            //ограничиваем движение точкой назначения
+            if (Math.abs((posDest.x - pos.x)) > PRECISION ||
+                    Math.abs((posDest.y - pos.y)) > PRECISION) {
+                //пересчитываем позицию объекта для следующей итерации
+                // к вектору позиции прибавляем вектор скорости при каждом обновлении экрана
+                System.out.println("(posDest.x - pos.x)=" +
+                        ((posDest.x - pos.x)) + "(posDest.y - pos.y)=" +
+                        ((posDest.y - pos.y)));
+
+                pos.add(v);
+            }
+        //если управление клавишами клавиатуры
+        } else{
+            //расчитываем координаты новой позиции, в т.ч. для отрицательных координат
+            pos.x += v.x + Gdx.graphics.getWidth() - img.getWidth();
+            //с зацикливанием при попытке картинки выйти за границы скрина
+            pos.x %= Gdx.graphics.getWidth() - img.getWidth();
+            pos.y += v.y + Gdx.graphics.getHeight() - img.getHeight();
+            pos.y %= Gdx.graphics.getHeight() - img.getHeight();
         }
     }
 
@@ -89,13 +119,63 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
+    public boolean keyDown(int keycode) {
+        super.keyDown(keycode);
+
+        //передаем управление от мыши к клавиатуре
+        keyControl = true;
+
+        switch (keycode){
+            case 19:
+                v.x = 0;
+                v.y = 1;
+                break;
+            case 20:
+                v.x = 0;
+                v.y = -1;
+                break;
+            case 21:
+                v.x = -1;
+                v.y = 0;
+                break;
+            case 22:
+                v.x = 1;
+                v.y = 0;
+                break;
+            default:
+                System.out.println("Not supported key for moving direction: " + keycode);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        super.keyUp(keycode);
+        //если клавиша отпущена, обнуляем вектор скорости
+        v.x = 0;
+        v.y = 0;
+        //возвращаем управление мыши
+        keyControl = false;
+
+        //обнуляем позицию
+        posDest.x = pos.x;
+        posDest.y = pos.y;
+
+        System.out.println("(posDest.x - pos.x)=" +
+                        (posDest.x - pos.x) + " (posDest.y - pos.y)=" +
+                (posDest.y - pos.y));
+
+        return false;
+    }
+
+    @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         super.touchDown(screenX, screenY, pointer, button);
         //устанавливаем координаты клика конечной позицией
         posDest.x = screenX;
         posDest.y = Gdx.graphics.getHeight() - screenY;//пересчет на лево-низ сетку координат
 
-        //System.out.println("posDest.x=" + posDest.x + "; " + "posDest.y=" + posDest.y);
+        System.out.println("posDest.x=" + posDest.x + "; " + "posDest.y=" + posDest.y);
 
         //***вычисляем вектор скорости***
         //сначала из копии вектора конечной позиции вычитаем вектор текущей и нормализуем его
@@ -106,10 +186,10 @@ public class MenuScreen extends BaseScreen {
         //System.out.println("/= v.len(); v.x=" + v.x + "; " + "v.y=" + v.y + "; v.len()=" + v.len());
 
         //добавляем коэффициент изменения величины скорости
-        v.scl(SPEED_FACTOR);
+        //v.scl(SPEED_FACTOR);
 
-        //System.out.println("v.scl(speedFactor); v.x=" + v.x + "; " + "v.y=" + v.y + "; v.len()=" + v.len());
-        //System.out.println();
+        System.out.println("v.scl(speedFactor); v.x=" + v.x + "; " + "v.y=" + v.y + "; v.len()=" + v.len());
+        System.out.println();
 
         return false;
     }
