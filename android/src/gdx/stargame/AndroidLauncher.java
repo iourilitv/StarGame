19c6d1d83a471a5eff7a1c1527aec7d879ca592a
iourilitv <iourilitv1965@gmail.com>
2019-11-08 09:17:43 +0300

    AndroidManifest.xml. Just replaced android:screenOrientation="landscape" with android:screenOrientation="portrait". And changed AndroidLauncher.

--- android/src/gdx/stargame/AndroidLauncher.java
+++ android/src/gdx/stargame/AndroidLauncher.java
@@ -5,19 +5,39 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-import gdx.stargame.puzzle.Puzzle1;
-import gdx.stargame.puzzle.Puzzle2;
-import gdx.stargame.stargame.StarGame;
+import gdx.stargame.lessons.lesson3.classfiles.StarGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
-
-//		initialize(new StarGame(), config);
-//        initialize(new Puzzle1(), config);
-		initialize(new Puzzle2(), config);
+		initialize(new StarGame(), config);
 	}
 }
+
+//***for .lessons.lesson3.classfiles.***
+
+	/*protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}*/
+
+//***for .puzzle.puzzle3.***
+//import gdx.stargame.puzzle.Puzzle1;
+//import gdx.stargame.puzzle.Puzzle2;
+		/*protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Puzzle2(), config);
+//        initialize(new Puzzle1(), config);
+	}*/
+
+//***for .stargame.StarGame***
+//import gdx.stargame.stargame.StarGame;
+	/*protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}*/
