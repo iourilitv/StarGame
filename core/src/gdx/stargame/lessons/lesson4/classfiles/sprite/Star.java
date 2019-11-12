00d2937c24f8eb581ed4817fc750759754f7d996
iourilitv <iourilitv1965@gmail.com>
2019-11-12 10:58:28 +0300

    Added commits into Star class of lesson 4 class files.

--- core/src/gdx/stargame/lessons/lesson4/classfiles/sprite/Star.java
+++ core/src/gdx/stargame/lessons/lesson4/classfiles/sprite/Star.java
@@ -7,6 +7,10 @@ import gdx.stargame.lessons.lesson4.classfiles.base.Sprite;
 import gdx.stargame.lessons.lesson4.classfiles.math.Rect;
 import gdx.stargame.lessons.lesson4.classfiles.math.Rnd;
 
+/**
+ * Спрайт звезды.
+ */
+
 public class Star extends Sprite {
 
     private Vector2 v = new Vector2();
@@ -15,6 +19,8 @@ public class Star extends Sprite {
     public Star(TextureAtlas atlas) {
         super(atlas.findRegion("star"));
         setHeightProportion(Rnd.nextFloat(0.01f, 0.0065f));
+        //генерируем случайные параметры вектора скорости звезды
+        // (из-за этого выглядит как падающий снег)
         float vy = Rnd.nextFloat(-0.005f, -0.001f);
         float vx = Rnd.nextFloat(-0.0005f, 0.0005f);
         v.set(vx, vy);
@@ -23,6 +29,7 @@ public class Star extends Sprite {
     @Override
     public void resize(Rect worldBounds) {
         this.worldBounds = worldBounds;
+        //генерируем случайные координаты позиции звезды
         float posX = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
         float posY = Rnd.nextFloat(worldBounds.getBottom(), worldBounds.getTop());
         pos.set(posX, posY);
@@ -31,6 +38,7 @@ public class Star extends Sprite {
     @Override
     public void update(float delta) {
         pos.add(v);
+        //проверяем не вышли ли за границы мира и выбрасываем звезду с обратной стороны
         checkBounds();
     }
 
