6c1e899d853ac7811fb0a235cceb704a2394e1f6
iourilitv <iourilitv1965@gmail.com>
2019-11-12 10:22:02 +0300

    Added topic comments of lesson 3 class files.

--- core/src/gdx/stargame/lessons/lesson3/classfiles/StarGame.java
+++ core/src/gdx/stargame/lessons/lesson3/classfiles/StarGame.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.Game;
 
 import gdx.stargame.lessons.lesson3.classfiles.screen.MenuScreen;
 
+/**
+ * Вебинар.20191028_GB-Разработка_игры_на_LibGDX.Преподаватель: Алексей Кутепов
+ * Урок 3. Разработка «каркаса» игры
+ * Создание каркаса проекта: игрок, астероиды, пули и их взаимодействие.
+ * @author Alexey Kutepov
+ * Матрица координатной сетки, приведение к единой координатной сетке,
+ * создание шаблона игрока Sprite.
+ */
 public class StarGame extends Game {
 
 	@Override
