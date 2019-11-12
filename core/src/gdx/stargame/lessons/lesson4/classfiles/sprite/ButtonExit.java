bad1f3df3d8c40d9bd96332a991cf71b1aba804a
iourilitv <iourilitv1965@gmail.com>
2019-11-12 10:23:01 +0300

    Added class files of lesson 4. All works correctly.

---
+++ core/src/gdx/stargame/lessons/lesson4/classfiles/sprite/ButtonExit.java
@@ -0,0 +1,26 @@
+package gdx.stargame.lessons.lesson4.classfiles.sprite;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import gdx.stargame.lessons.lesson4.classfiles.base.ScaledTouchUpButton;
+import gdx.stargame.lessons.lesson4.classfiles.math.Rect;
+
+public class ButtonExit extends ScaledTouchUpButton {
+
+    public ButtonExit(TextureAtlas atlas) {
+        super(atlas.findRegion("btExit"));
+    }
+
+    @Override
+    public void action() {
+        Gdx.app.exit();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(0.15f);
+        setRight(worldBounds.getRight() - 0.05f);
+        setBottom(worldBounds.getBottom() + 0.05f);
+    }
+}
--- desktop/src/gdx/stargame/desktop/DesktopLauncher.java
+++ desktop/src/gdx/stargame/desktop/DesktopLauncher.java
@@ -3,7 +3,8 @@ package gdx.stargame.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import gdx.stargame.lessons.lesson3.hw.StarGame;
+//import gdx.stargame.lessons.lesson3.hw.StarGame;
+import gdx.stargame.lessons.lesson4.classfiles.StarGame;
 
 public class DesktopLauncher {
     public static void main (String[] arg) {
