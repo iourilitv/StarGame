5a431a31535c3100e017633e85d7e7a9f182fd95
iourilitv <iourilitv1965@gmail.com>
2019-11-22 13:55:40 +0300

    Added some comments into Ship class of lesson 7 class files. All works correctly.

--- core/src/gdx/lessons/lesson7/classfiles/base/Ship.java
+++ core/src/gdx/lessons/lesson7/classfiles/base/Ship.java
@@ -29,6 +29,7 @@ public abstract class Ship extends Sprite {
     protected float reloadInterval = 0f;
     protected float reloadTimer = 0f;
 
+    //инициируем переменные интервала и таймера для анимации попадания снаряда в корабль
     protected float animateInterval = 0.05f;
     protected float animateTimer = animateInterval;
 
@@ -46,8 +47,11 @@ public abstract class Ship extends Sprite {
             reloadTimer = 0f;
             shoot();
         }
+        //инкрементируем таймер анимации попадания снаряда в корабль
         animateTimer += delta;
+        //если таймер стал больше интервала
         if (animateTimer > animateInterval) {
+            //показываем основную картинку корабля
             frame = 0;
         }
         pos.mulAdd(v, delta);
@@ -55,16 +59,26 @@ public abstract class Ship extends Sprite {
 
     @Override
     public void destroy() {
+        //вызываем метод взрыва
         boom();
         super.destroy();
     }
 
+    /**
+     * Метод расчета повреждения корабля при попадании снаряда или столкновения с чужим объектом
+     * @param damage - размер нанесенного кораблю ущерба
+     */
     public void damage(int damage) {
+        //уменьшаем значение здоровья корабля на размер нанесенного ущерба
         hp -= damage;
+        //если здоровье кончилось
         if (hp <= 0) {
+            //вызываем метод уничтожения корабля
             destroy();
         }
+        //сбрасываем таймер анимации взрыва
         animateTimer = 0f;
+        //показываем светлую картинку корабля
         frame = 1;
     }
 
@@ -78,8 +92,13 @@ public abstract class Ship extends Sprite {
         bullet.set(this, bulletRegion, pos, bulletV, bulletHeight, worldBounds, damage);
     }
 
+    /**
+     * Метод отрабатывает взрыв корабля при попадании в него чего-либо
+     */
     protected void boom() {
+        //вызываем из пула объект взрыва
         Explosion explosion = explosionPool.obtain();
+        //устанавливаем настройки объекта взрыва(вектор его позиции и высоту его картинки)
         explosion.set(pos, getHeight());
     }
 }
