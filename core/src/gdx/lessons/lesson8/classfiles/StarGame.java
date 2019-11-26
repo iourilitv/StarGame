d5e913a197f3beba35828d5644b8f75cf312f07d
iourilitv <iourilitv1965@gmail.com>
2019-11-26 07:44:06 +0300

    Added topic comments into StarGame class and some explaining comments into Font class of class files of lesson 8. All works correctly.

--- core/src/gdx/lessons/lesson8/classfiles/StarGame.java
+++ core/src/gdx/lessons/lesson8/classfiles/StarGame.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.Game;
 
 import gdx.lessons.lesson8.classfiles.screen.MenuScreen;
 
+/**
+ * Вебинар.20191028_GB-Разработка_игры_на_LibGDX.Преподаватель: Алексей Кутепов
+ * Урок 8. Финальная доработка проекта
+ * Доработка всех компонентов. Сборка APK-файла. Тестирование на устройстве.
+ * На видео: Вывод текста с помощью BitMap(сборщик runnable-hiero.jar). Режим паузы. Подсчет отчков. Уровни игры. Сборка APK-файла.
+ * @author Alexey Kutepov
+ *
+ */
 public class StarGame extends Game {
 
 	@Override
