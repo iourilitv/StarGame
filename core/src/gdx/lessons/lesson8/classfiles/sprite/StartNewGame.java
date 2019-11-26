bc5559606df4133d8112b0a3c0247d0f519fd39c
iourilitv <iourilitv1965@gmail.com>
2019-11-26 09:48:18 +0300

    Added some explaining comments into StartNewGame class of class files of lesson 8. All works correctly.

--- core/src/gdx/lessons/lesson8/classfiles/sprite/StartNewGame.java
+++ core/src/gdx/lessons/lesson8/classfiles/sprite/StartNewGame.java
@@ -6,23 +6,42 @@ import gdx.lessons.lesson8.classfiles.base.ScaledTouchUpButton;
 import gdx.lessons.lesson8.classfiles.math.Rect;
 import gdx.lessons.lesson8.classfiles.screen.GameScreen;
 
+/**
+ * Класс кнопки "NewGame" для организации режима игры "начать новую игру".
+ */
 public class StartNewGame extends ScaledTouchUpButton {
-
+    //передаем объект скрина(экрана)
     private GameScreen screen;
 
+    /**
+     * Конструктор
+     * @param atlas - атлас текстуры
+     * @param screen - объект скрина(экрана)
+     */
     public StartNewGame(TextureAtlas atlas, GameScreen screen) {
+        //передаем в родительский класс текструру-регион картинки кнопки "NewGame"
         super(atlas.findRegion("button_new_game"));
         this.screen = screen;
     }
 
+    /**
+     * Метод установки размера и позиции кнопки
+     * @param worldBounds - прямоугольник игровго мира в мировых координатах
+     */
     @Override
     public void resize(Rect worldBounds) {
+        //устанавливаем размер кнопки по ее заданной высоте
         setHeightProportion(0.05f);
+        //уснонавливаем низ кнопки с отступом вниз от центра игрового мира
         setBottom(-0.08f);
     }
 
+    /**
+     * Метод отбрабатывает событие касание или клика на кнопку "NewGame"
+     */
     @Override
     public void action() {
+        //вызываем метод в сткрине, чтобы начать новую игру
         screen.startNewGame();
     }
 }
